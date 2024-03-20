417a0eba3b171906d079555a7f6ac7c4d93f1834
Akshat <imakshat.goswami@gmail.com>
2024-03-20 18:26:34 +0530

    Java Code 2.0

---
+++ code.java
@@ -0,0 +1,6 @@
+public class code{
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+        System.out.println("This is My First Java Code on GitHub");
+    }
+}
